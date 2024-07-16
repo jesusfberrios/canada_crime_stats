fff6e95c3b9ca8fcdbe0dc34fc308c98792ad8a9
Jesus Berrios <81935591+jesusfberrios@users.noreply.github.com>
2024-07-16 11:16:50 -0400

    Add files via upload

---
+++ init_database.py
@@ -0,0 +1,32 @@
+from sqlalchemy import create_engine
+import pandas as pd
+import os
+
+# Define MySQL connection parameters
+db_user = os.environ['DBUSER']
+db_pass = os.environ['DBPASS']
+db_host = 'localhost'
+db_name = 'canada_stats'
+
+# Create SQLAlchemy engine
+engine = create_engine(f'mysql+mysqlconnector://{db_user}:{db_pass}@{db_host}/{db_name}')
+
+# Test the connection
+try:
+    engine.connect()
+    print("Connected successfully!")
+except Exception as e:
+    print("Connection failed:", e)
+
+
+df_h = pd.read_csv('./homicides.csv')
+df_h.to_sql('homicides', con=engine, if_exists='replace', index=False)
+
+df_pc = pd.read_csv('./persons_charged.csv')
+df_pc.to_sql('persons_charged', con=engine, if_exists='replace', index=False)
+
+df_pc = pd.read_csv('./provinces.csv')
+df_pc.to_sql('provinces', con=engine, if_exists='replace', index=False)
+
+# Close the connection
+engine.dispose()
