cf0777b7de585425c543943ba1a7d5aa9bf336c8
jesusfberrios <jesusfberrios@gmail.com>
2024-07-18 00:14:50 -0400

    uopdate init scripts and app.py

--- web_app/init_app.sh
+++ web_app/init_app.sh
@@ -8,14 +8,12 @@ unzip awscliv2.zip
 sudo ./aws/install
 git clone https://github.com/jesusfberrios/canada_crime_stats.git
 #pip3 install sqlalchemy mysql-connector-python pandas 
-pip3 install -r  canada_crime_stats/web_app/requirements.txt
+pip3 install -r  canada_crime_stats/web_app/requirements_web.txt
 
 # Get credentials from secrets
 export DB_USER=$(aws secretsmanager get-secret-value --secret-id /mysql/credentials --query SecretString | jq -r 'fromjson' | jq -r '.DB_USER')
 export DB_PASSWORD=$(aws secretsmanager get-secret-value --secret-id /mysql/credentials --query SecretString | jq -r 'fromjson' | jq -r '.DB_PASSWORD')
 export DB_HOST=$(aws secretsmanager get-secret-value --secret-id /mysql/credentials --query SecretString | jq -r 'fromjson' | jq -r '.DB_HOST')
 
-# Get data
-git clone https://github.com/jesusfberrios/canada_crime_stats.git
-cd canada_crime_stats/db_install/
-python3 init_database.py
+# Turn on app
+python3 canada_crime_stats/web_app/app.py
