ce91c03fc0cd562a58c6a3899e06aaa0ffe93bc3
jesusfberrios <jesusfberrios@gmail.com>
2024-07-21 10:21:20 -0400

    init scripts updates

--- db_install/user_data_db.sh
+++ db_install/user_data_db.sh
@@ -2,5 +2,5 @@
 
 # Clone repository and execute database init script
 git clone https://github.com/jesusfberrios/canada_crime_stats.git
-chmod +x install -r  canada_crime_stats/db_install/init_db.sh
+chmod +x canada_crime_stats/db_install/init_db.sh
 canada_crime_stats/db_install/init_db.sh
--- web_app/user_data_web.sh
+++ web_app/user_data_web.sh
@@ -2,5 +2,5 @@
 
 # Clone repository and execute database init script
 git clone https://github.com/jesusfberrios/canada_crime_stats.git
-chmod +x install -r  canada_crime_stats/web_app/init_web.sh
+chmod +x canada_crime_stats/web_app/init_web.sh
 canada_crime_stats/web_app/init_web.sh
