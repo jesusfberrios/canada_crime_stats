609498ec81fa6e6fceed2f75d10de7dec52d71d3
jesusfberrios <jesusfberrios@gmail.com>
2024-07-21 10:26:32 -0400

    init scripts update

--- web_app/user_data_web.sh
+++ web_app/user_data_web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Clone repository and execute database init script
+# Clone repository and execute web init script
 git clone https://github.com/jesusfberrios/canada_crime_stats.git
 chmod +x canada_crime_stats/web_app/init_web.sh
 canada_crime_stats/web_app/init_web.sh
